9b1a1a8097a6b9aaa2b780eb8300dc8d9d949aeb
Terry <raising.2201@gmail.com>
2023-12-01 15:51:24 +0800

    修正 gui 介面 bug

--- stock/company-income.py
+++ stock/company-income.py
@@ -9,13 +9,22 @@ import PySimpleGUI as sg
 bar_format = "{l_bar}{bar}{r_bar}"
 tqdm.pandas(bar_format=bar_format)
 
+def parse_range(input_range):
+    result = []
+    for part in input_range:
+        if '-' in part:
+            start, end = map(int, part.split('-'))
+            result.extend(range(start, end + 1))
+        else:
+            result.append(int(part))
+    return result
+
 def get_company_data(company_ids, year_range, month_range):
     data = []
 
     total_iterations = len(company_ids) * len(year_range) * len(month_range)
 
-    for combination in tqdm(product(company_ids, year_range, month_range),
-                            total=total_iterations, desc="Processing", colour='green'):
+    for combination in tqdm(product(company_ids, year_range, month_range), total=total_iterations, desc="Processing", colour='green'):
         company_id, year, month = combination
         url = f'https://mops.twse.com.tw/nas/t21/sii/t21sc03_{year}_{month}_0.html'
         company_data = get_company_basic_data(company_id, url)
@@ -67,43 +76,92 @@ def get_company_basic_data(company_id, url):
 
 def print_results(company_data):
     if company_data:
-        sorted_data = sorted(company_data, key=lambda x: x['公司代號'])
+        sorted_data = sorted(company_data, key=lambda x: (x['公司代號'], x['月份']))
 
-        table_basic = PrettyTable()
-        table_basic.field_names = ['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)', '月份']
+        layout = [
+            [sg.Text('公司代號（以逗號分隔）'), sg.InputText(key='company_ids')],
+            [sg.Text('年份範圍（以逗號分隔或使用範圍符號 "-"）'), sg.InputText(key='year_range')],
+            [sg.Text('月份範圍（以逗號分隔或使用範圍符號 "-"）'), sg.InputText(key='month_range')],
+            [sg.Button('確認')],
+            [sg.Table(values=[], headings=['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)', '月份'],
+                      auto_size_columns=False, justification='right', key='table',
+                      col_widths=[15, 30, 15, 15, 15, 20, 20, 15],
+                      row_height=30, display_row_numbers=False, bind_return_key=True, enable_events=True, num_rows=20)],
+            [sg.Text('每年度的平均當月營收:', key='average_text')],
+        ]
 
-        data_rows = []
-        for company_id, group in groupby(sorted_data, key=lambda x: x['公司代號']):
-            group = list(group)
+        window = sg.Window('公司資料查詢工具', layout, resizable=True, finalize=True)
 
-            for i, company in enumerate(group):
-                values_basic = [company.get(label, '') for label in table_basic.field_names[:-1]]
-                values_basic.append(company.get('月份', ''))
-                data_rows.append(values_basic)
+        while True:
+            event, values = window.read()
 
-            yearly_data = {year: [] for year in set(int(company['月份'].split('-')[0]) for company in group)}
-            for company in group:
-                year = int(company['月份'].split('-')[0])
-                yearly_data[year].append(float(company['當月營收'].replace(',', '')))
+            if event == sg.WINDOW_CLOSED:
+                break
+            elif event == '確認':
+                # Parsing input
+                company_ids_input = [company_id.strip() for company_id in values['company_ids'].split(',')]
+                year_range_input = values['year_range'].split(',')
+                month_range_input = values['month_range'].split(',')
 
-            data_rows.append(["", "", "", "", "", "", "", ""])  # Add an empty row between companies
+                # Converting to integers
+                year_range = parse_range(year_range_input)
+                month_range = parse_range(month_range_input)
 
-            for year, revenues in yearly_data.items():
-                average_revenue = sum(revenues) / len(revenues)
-                data_rows.append([f"{year}年平均當月營收:", "", "", "", "", "", f"{average_revenue:.2f}", ""])
+                # Calling the function to get company data
+                company_data = get_company_data(company_ids_input, year_range, month_range)
 
-        return data_rows
+                # Sorting and printing company data
+                sorted_data = sorted(company_data, key=lambda x: (x['公司代號'], x['月份']))
+
+                table_data = []
+                yearly_data = {year: [] for year in range(year_range[0], year_range[1] + 1)}
+
+                for company_id, group in groupby(sorted_data, key=lambda x: x['公司代號']):
+                    group = list(group)
+
+                    for i, company in enumerate(group):
+                        values_basic = [company.get(label, '') for label in ['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)']]  # 不包含 '月份'
+                        values_basic.append(company.get('月份', ''))  # 添加 '月份'
+                        table_data.append(values_basic)
+
+                        if i == len(group) - 1:
+                            # 添加空行，用於區分不同公司的數據
+                            table_data.append([""] * len(table_data[0]))
+
+                        # 計算每年度的平均值
+                        year = int(company['月份'].split('-')[0])
+                        yearly_data[year].append(float(company['當月營收'].replace(',', '')))
+
+                    # 插入每年度的平均值行
+                    for year, revenues in yearly_data.items():
+                        if revenues:  # 避免除以零
+                            average_revenue = sum(revenues) / len(revenues)
+                            table_data.append(["", "", f"{average_revenue:.2f}", "", "", "", "", f"{year}年 平均值"])
+
+                window['table'].update(values=table_data)
+
+                # 輸出每年度的平均值
+                avg_text = ""
+                for year, revenues in yearly_data.items():
+                    if revenues:  # 避免除以零
+                        average_revenue = sum(revenues) / len(revenues)
+                        avg_text += f"{year}年: {average_revenue:.2f}\n"
+
+                window['average_text'].update(avg_text)
+
+        window.close()
 
 # PySimpleGUI used for creating the graphical interface
 layout = [
     [sg.Text('公司代號（以逗號分隔）'), sg.InputText(key='company_ids')],
-    [sg.Text('年份範圍（以逗號分隔）'), sg.InputText(key='year_range')],
-    [sg.Text('月份範圍（以逗號分隔）'), sg.InputText(key='month_range')],
-    [sg.Button('確認'), sg.Button('退出')],
+    [sg.Text('年份範圍（以逗號分隔或使用範圍符號 "-"）'), sg.InputText(key='year_range')],
+    [sg.Text('月份範圍（以逗號分隔或使用範圍符號 "-"）'), sg.InputText(key='month_range')],
+    [sg.Button('確認')],
     [sg.Table(values=[], headings=['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)', '月份'],
-             auto_size_columns=False, justification='right', key='table',
+              auto_size_columns=False, justification='right', key='table',
               col_widths=[15, 30, 15, 15, 15, 20, 20, 15],
-              row_height=30, display_row_numbers=False, bind_return_key=True, enable_events=True, num_rows=20)],
+              row_height=30, display_row_numbers=False, bind_return_key=True, enable_events=True, num_rows=20)]
+    
 ]
 
 window = sg.Window('公司資料查詢工具', layout, resizable=True, finalize=True)
@@ -111,7 +169,7 @@ window = sg.Window('公司資料查詢工具', layout, resizable=True, finalize=
 while True:
     event, values = window.read()
 
-    if event == sg.WINDOW_CLOSED or event == '退出':
+    if event == sg.WINDOW_CLOSED:
         break
     elif event == '確認':
         # Parsing input
@@ -120,16 +178,43 @@ while True:
         month_range_input = values['month_range'].split(',')
 
         # Converting to integers
-        year_range = [int(year) for year in year_range_input] if year_range_input[0] else [int(year_range_input[0])]
-        month_range = [int(month) for month in month_range_input] if month_range_input[0] else [int(month_range_input[0])]
+        year_range = parse_range(year_range_input)
+        month_range = parse_range(month_range_input)
 
         # Calling the function to get company data
         company_data = get_company_data(company_ids_input, year_range, month_range)
 
-        # Printing company data
-        table_data = print_results(company_data)
+        # Sorting and printing company data
+        sorted_data = sorted(company_data, key=lambda x: (x['公司代號'], x['月份']))
 
-        # Update the table in the PySimpleGUI window
-        window['table'].update(values=table_data)
+        table_data = []
+        yearly_data = {year: [] for year in range(year_range[0], year_range[1] + 1)}
+
+    for company_id, group in groupby(sorted_data, key=lambda x: x['公司代號']):
+        group = list(group)
+
+        for i, company in enumerate(group):
+            values_basic = [company.get(label, '') for label in ['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)']]  # 不包含 '月份'
+            values_basic.append(company.get('月份', ''))  # 添加 '月份'
+            table_data.append(values_basic)
+
+            if i == len(group) - 1:
+                # 添加空行，用於區分不同公司的數據
+                table_data.append([""] * len(table_data[0]))
+
+        # 計算每年度的平均值
+        yearly_data = {year: [] for year in range(year_range[0], year_range[1] + 1)}
+        for company in group:
+            year = int(company['月份'].split('-')[0])
+            yearly_data[year].append(float(company['當月營收'].replace(',', '')))
+
+        # 插入每年度的平均值行
+        for year, revenues in yearly_data.items():
+            if revenues:  # 避免除以零
+                average_revenue = sum(revenues) / len(revenues)
+                table_data.append(["", "", f"{average_revenue:.2f}", "", "", "", "", f"{year}年 平均值"])
+
+    window['table'].update(values=table_data)
+
 
 window.close()
