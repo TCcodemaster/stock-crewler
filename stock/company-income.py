06d64e51351a7ccaa6cbde892a26836329bfd9e3
Terry <raising.2201@gmail.com>
2023-12-01 13:07:05 +0800

    add PySimpleGUI

--- stock/company-income.py
+++ stock/company-income.py
@@ -4,6 +4,7 @@ from prettytable import PrettyTable
 from itertools import product
 from tqdm import tqdm
 from itertools import groupby
+import PySimpleGUI as sg
 
 bar_format = "{l_bar}{bar}{r_bar}"
 tqdm.pandas(bar_format=bar_format)
@@ -11,11 +12,9 @@ tqdm.pandas(bar_format=bar_format)
 def get_company_data(company_ids, year_range, month_range):
     data = []
 
-    # 估算迴圈的總長度
-    total_iterations = len(company_ids) * len(range(year_range[0], year_range[1] + 1)) * len(range(month_range[0], month_range[1] + 1))
+    total_iterations = len(company_ids) * len(year_range) * len(month_range)
 
-    # 封裝成 tqdm
-    for combination in tqdm(product(company_ids, range(year_range[0], year_range[1] + 1), range(month_range[0], month_range[1] + 1)),
+    for combination in tqdm(product(company_ids, year_range, month_range),
                             total=total_iterations, desc="Processing", colour='green'):
         company_id, year, month = combination
         url = f'https://mops.twse.com.tw/nas/t21/sii/t21sc03_{year}_{month}_0.html'
@@ -30,24 +29,19 @@ def get_company_data(company_ids, year_range, month_range):
 def get_company_basic_data(company_id, url):
     data = {}
 
-    # 發送 HTTP 請求
     response = requests.get(url)
 
-    # 檢查請求是否成功
     if response.status_code == 200:
-        # 使用 BeautifulSoup 解析 HTML 內容
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # 找到指定的表格
         target_table = soup.find('table')
 
-        # 提取表格中的資料
         if target_table:
-            rows = target_table.find_all('tr')[2:]  # 忽略前兩行，因為它們是表頭
+            rows = target_table.find_all('tr')[2:]
 
             for row in rows:
                 columns = row.find_all('td')
-                if columns:  # 確保這是一行資料，而不是空行
+                if columns:
                     fetched_company_id = columns[0].text.strip()
                     if fetched_company_id == company_id:
                         company_name = columns[1].text.strip().encode('latin-1').decode('big5', 'ignore')
@@ -67,72 +61,75 @@ def get_company_basic_data(company_id, url):
                             '去年同月增減(%)': last_year_growth_rate
                         }
 
-                        break  # 移到這裡，確保只在找到符合公司代號的資料後中斷迴圈
+                        break
 
     return data
 
-
 def print_results(company_data):
     if company_data:
-        # 先按公司代號排序
         sorted_data = sorted(company_data, key=lambda x: x['公司代號'])
 
-        # 印出表格的標籤
         table_basic = PrettyTable()
         table_basic.field_names = ['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)', '月份']
 
-        # 顯示每筆資料
+        data_rows = []
         for company_id, group in groupby(sorted_data, key=lambda x: x['公司代號']):
             group = list(group)
 
             for i, company in enumerate(group):
-                # 顯示基本資料
-                values_basic = [company.get(label, '') for label in table_basic.field_names[:-1]]  # 不包含 '月份'
-                values_basic.append(company.get('月份', ''))  # 添加 '月份'
-                table_basic.add_row(values_basic)
+                values_basic = [company.get(label, '') for label in table_basic.field_names[:-1]]
+                values_basic.append(company.get('月份', ''))
+                data_rows.append(values_basic)
 
-                if i == len(group) - 1:
-                    print(f"基本資料 (公司代號: {company_id}):")
-                    print(table_basic)
-
-            # 計算每年度的平均值
-            yearly_data = {year: [] for year in range(year_range[0], year_range[1] + 1)}
+            yearly_data = {year: [] for year in set(int(company['月份'].split('-')[0]) for company in group)}
             for company in group:
                 year = int(company['月份'].split('-')[0])
                 yearly_data[year].append(float(company['當月營收'].replace(',', '')))
 
-            # 輸出每年度的平均值
-            print(f"每年度的平均當月營收:")
+            data_rows.append(["", "", "", "", "", "", "", ""])  # Add an empty row between companies
+
             for year, revenues in yearly_data.items():
                 average_revenue = sum(revenues) / len(revenues)
-                print(f"{year}年: {average_revenue:.2f}")
+                data_rows.append([f"{year}年平均當月營收:", "", "", "", "", "", f"{average_revenue:.2f}", ""])
 
-            # 清空表格
-            table_basic.clear_rows()
+        return data_rows
 
-# 主程式碼
+# PySimpleGUI used for creating the graphical interface
+layout = [
+    [sg.Text('公司代號（以逗號分隔）'), sg.InputText(key='company_ids')],
+    [sg.Text('年份範圍（以逗號分隔）'), sg.InputText(key='year_range')],
+    [sg.Text('月份範圍（以逗號分隔）'), sg.InputText(key='month_range')],
+    [sg.Button('確認'), sg.Button('退出')],
+    [sg.Table(values=[], headings=['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)', '月份'],
+             auto_size_columns=False, justification='right', key='table',
+              col_widths=[15, 30, 15, 15, 15, 20, 20, 15],
+              row_height=30, display_row_numbers=False, bind_return_key=True, enable_events=True, num_rows=20)],
+]
+
+window = sg.Window('公司資料查詢工具', layout, resizable=True, finalize=True)
+
 while True:
-    user_input = input('請輸入輸入code，或輸入 "q" 退出: ')
-    if user_input.lower() == 'q':
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == '退出':
         break
-    else:
-        # 解析輸入
-        input_parts = user_input.split(')')
-        company_ids_input = [company_id.strip() for company_id in input_parts[0].replace('id(', '').split(',')]
-        year_range_input = input_parts[1].replace('year(', '').split(',')
-        month_range_input = input_parts[2].replace('month(', '').replace(')', '').split(',')
+    elif event == '確認':
+        # Parsing input
+        company_ids_input = [company_id.strip() for company_id in values['company_ids'].split(',')]
+        year_range_input = values['year_range'].split(',')
+        month_range_input = values['month_range'].split(',')
 
-        # 轉換為整數
-        year_range = [int(year) for year in year_range_input]
-        month_range = [int(month) for month in month_range_input]   
+        # Converting to integers
+        year_range = [int(year) for year in year_range_input] if year_range_input[0] else [int(year_range_input[0])]
+        month_range = [int(month) for month in month_range_input] if month_range_input[0] else [int(month_range_input[0])]
 
-        # 輸出解析後的值（可選）
-        print("公司代號:", company_ids_input)
-        print("年份範圍:", year_range)
-        print("月份範圍:", month_range)
-
-        # 呼叫函式取得公司資料
+        # Calling the function to get company data
         company_data = get_company_data(company_ids_input, year_range, month_range)
 
-        # 印出公司資料
-        print_results(company_data)
+        # Printing company data
+        table_data = print_results(company_data)
+
+        # Update the table in the PySimpleGUI window
+        window['table'].update(values=table_data)
+
+window.close()
