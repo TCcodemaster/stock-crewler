6faaebd613bf1467bfef3e1de92ded09640b8d1b
Terry <raising.2201@gmail.com>
2023-12-04 14:40:31 +0800

    增加 自動填入功能

--- stock/company-income.py
+++ stock/company-income.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 import PySimpleGUI as sg
 from matplotlib.font_manager import FontProperties as font
 from tqdm import tqdm
+import os  # 引入 os 模組
 
-font1 = font(fname=r"c:\Users\Terry Chien\Downloads\Noto_Sans_TC\NotoSansTC-VariableFont_wght.ttf")
+# 指定檔案路徑
+input_log_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'input_log.txt')
+
+# 其他程式碼不變
+
+font1 = font(fname=r"C:\Users\raising.2201.DESKTOP-OIFBBUJ\Downloads\Noto_Sans_TC\NotoSansTC-VariableFont_wght.ttf")
 
 bar_format = "{l_bar}{bar}{r_bar}"
 tqdm.pandas(bar_format=bar_format)
@@ -80,33 +86,79 @@ def get_company_basic_data(company_id, url):
 
     return data
 
-# PySimpleGUI used for creating the graphical interface
+
+
+def read_input_log(file_path):
+    # 讀取過去的五筆紀錄
+    input_log = []
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            for line in lines[-5:]:  # 只取最後五筆
+                input_log.append(line.strip())
+    except FileNotFoundError:
+        pass
+    return input_log
+
+def save_input_log(file_path, input_log):
+    # 將最新的輸入保存到檔案
+    with open(file_path, 'a') as file:
+        file.write('\n'.join(input_log) + '\n')
+
+def parse_input_record(record):
+    # 解析輸入紀錄為字典
+    parsed_record = {}
+    parts = record.split(',')
+    for part in parts:
+        key, value = map(str.strip, part.split(':'))
+        parsed_record[key] = value
+    return parsed_record
+
+# PySimpleGUI 布局
 layout = [
+    [sg.Text('請選擇過去的五筆紀錄')],[sg.Button('填入')],
+    [sg.DropDown(values=read_input_log(input_log_file), key='saved_inputs')],
     [sg.Text('公司代號（以逗號分隔）'), sg.InputText(key='company_ids')],
     [sg.Text('年份範圍（以逗號分隔或使用範圍符號 "-"）'), sg.InputText(key='year_range')],
     [sg.Text('月份範圍（以逗號分隔或使用範圍符號 "-"）'), sg.InputText(key='month_range')],
-    [sg.Button('確認'),sg.Button('生成折線圖', key='plot_button'),sg.Button('離開', key='leave')],
+    [sg.Button('確認'), sg.Button('生成折線圖', key='plot_button'), sg.Button('離開', key='leave')],
     [sg.Table(values=[], headings=['公司代號', '公司名稱', '當月營收', '上月營收', '去年當月營收', '上月比較增減(%)', '去年同月增減(%)', '月份'],
               auto_size_columns=False, justification='right', key='table',
               col_widths=[15, 30, 15, 15, 15, 20, 20, 15],
               row_height=30, display_row_numbers=False, bind_return_key=True, enable_events=True, num_rows=20)],
-    
 ]
 
 window = sg.Window('公司資料查詢工具', layout, resizable=True, finalize=True)
 
+
 while True:
     event, values = window.read()
 
-    if event == sg.WINDOW_CLOSED:
+    if event == sg.WINDOW_CLOSED or event == 'leave':
+        if values is not None:
+            # 在退出時保存輸入紀錄到檔案
+            input_log = read_input_log(input_log_file)
+            current_input = f"company_ids:{values['company_ids']}, year_range:{values['year_range']}, month_range:{values['month_range']}"
+            if current_input not in input_log:
+                input_log.append(current_input)
+                save_input_log(input_log_file, input_log)
         break
-    elif event == 'leave':
-        break
+    elif event == '填入':
+                # 解析選擇的過去紀錄
+        selected_input = values['saved_inputs']
+        if selected_input:
+            selected_input_dict = parse_input_record(selected_input)
+
+            # Update the values for each input field individually
+            for key, value in selected_input_dict.items():
+                window[key].update(value)
     elif event == '確認':
+
         # Parsing input
         company_ids_input = [company_id.strip() for company_id in values['company_ids'].split(',')]
         year_range_input = values['year_range'].split(',')
         month_range_input = values['month_range'].split(',')
+        
 
         # Converting to integers
         year_range = parse_range(year_range_input)
